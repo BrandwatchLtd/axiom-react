29c7d706c7faba9256b85a248f784e8277259c23
Sammy Braun <sammy@brandwatch.com>
2016-08-11 09:17:25 +0200

    Updated webpack structure

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-const Alias = require('./utils/webpack-alias-plugin');
-var path = require('path');
-
 module.exports = {
   server: {
     port: 4000,
@@ -9,10 +6,5 @@ module.exports = {
   output: {
     folderName: 'lib',
     js: 'axiom.js',
-  },
-  webpack: {
-    aliases: [
-      new Alias(/^bw-axiom/, (resolve) => resolve.replace(/^bw-axiom(.*)/, path.resolve(__dirname, 'docs') + '$1')),
-    ],
   },
 };
