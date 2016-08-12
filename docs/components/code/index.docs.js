2cddf160b77eaaa6e9ad62975391bde4ae95a4d4
Sammy Braun <sammy@brandwatch.com>
2016-08-12 16:53:23 +0200

    Added new Code component

---
+++ docs/components/code/index.docs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const title = 'Code';
+export const route = '/components/code';
+export const Description = () => (
+  <div>
+    The code component
+  </div>
+);
+
+export { default as Example } from './Example';
