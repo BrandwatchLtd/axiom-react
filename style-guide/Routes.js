d2ac78f6af2b96bdf62037570c397ef0f99290a8
Sammy Braun <sammy@brandwatch.com>
2016-08-12 15:08:23 +0200

    Flattended style-guide structure and simplified some stuff

--- style-guide/Routes.js
+++ style-guide/Routes.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from '../App';
-import Doc from '../Doc';
-
-// ToDO: Instead of IndexRoute use IndexRedirect to first best path from docStructure
+import App from './App';
+import Doc from './Doc';
 
 export default (
   <Route component={ App } path="/">
