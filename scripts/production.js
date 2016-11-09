fb69c50ae822cf77e2fdc2c1309c2f7d3998c539
Sammy Braun <sammy@brandwatch.com>
2016-11-09 14:33:28 +0100

    Serving static html file for development

--- scripts/production.js
+++ scripts/production.js
@@ -1,14 +1,11 @@
 const clean = require('./clean');
 const buildClient = require('./buildClient');
-const buildServer = require('./buildServer');
 const server = require('./server');
-const config = require('../config');
 
 function production() {
   clean()
-    .then(() => buildServer())
     .then(() => buildClient())
-    .then(() => server(config.output.styleGuide.serverProdJSFilename));
+    .then(() => server());
 }
 
 production();
