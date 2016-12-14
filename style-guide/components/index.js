d1c75ac38b74716109c67539831fb0cc8fd7716a
Harry Hogg <harry@hogg.io>
2016-12-14 22:10:35 +0000

    Stripped out Color swatch stuff until design iteration

--- style-guide/components/index.js
+++ style-guide/components/index.js
@@ -1,5 +1,4 @@
 export { CodeSnippet } from './CodeSnippet';
-export { default as ColorSwatch } from './ColorSwatch/ColorSwatch';
 export { default as DemoBox } from './DemoBox/DemoBox';
 export { Example } from './Example';
 export { Snippet } from './Example';
