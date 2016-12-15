9d5745a333a5c3a99654e4911dc377d949106604
Travis CI <sammy@pampelmuse.stuttgart.brandwatch.net>
2016-12-15 17:20:45 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -50,6 +50,7 @@ fi
 git add --all .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
+
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 # ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 # ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
