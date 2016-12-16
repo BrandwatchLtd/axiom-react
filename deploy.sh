f1b0c0c5e179d2d3478ac6023a261fe4fd1bc674
Sammy Braun <sammy@brandwatch.com>
2016-12-16 08:58:08 +0100

    Fixed publish key path

--- deploy.sh
+++ deploy.sh
@@ -57,10 +57,10 @@ git commit -m "Deploy to GitHub Pages: ${SHA}"
 # ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 # ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 # openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../publish-key.enc -out publish-key -d
-openssl aes-256-cbc -K $encrypted_5890ad0da7e1_key -iv $encrypted_5890ad0da7e1_iv -in publish-key.enc -out publish-key -d
-chmod 600 ../publish-key
+openssl aes-256-cbc -K $encrypted_5890ad0da7e1_key -iv $encrypted_5890ad0da7e1_iv -in ../publish-key.enc -out publish-key -d
+chmod 600 publish-key
 eval `ssh-agent -s`
-ssh-add ../publish-key
+ssh-add publish-key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
