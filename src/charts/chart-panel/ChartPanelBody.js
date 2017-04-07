0d3010de2ac0a90da3d0d1bdd61190711a48d66f
Michael Sorhaindo <pmsorhaindo@gmail.com>
2017-04-07 17:29:16 +0100

    fix(Chart): adds alignment properties for chart body

--- src/charts/chart-panel/ChartPanelBody.js
+++ src/charts/chart-panel/ChartPanelBody.js
@@ -1,15 +1,24 @@
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
 export default class ChartPanelBody extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    horizontalAlign: PropTypes.oneOf(['start', 'middle', 'end']),
+    verticalAlign: PropTypes.oneOf(['start', 'middle', 'end', 'around', 'between']),
   };
 
   render() {
+    const { horizontalAlign, verticalAlign } = this.props;
+    const classes = classnames('ax-chart-panel__body', {
+      [`ax-chart-panel__body--vertical-${verticalAlign}`]: verticalAlign,
+      [`ax-chart-panel__body--horizontal-${horizontalAlign}`]: horizontalAlign,
+    });
+
     const { children, ...rest } = this.props;
 
     return (
-      <div { ...rest } className="ax-chart-panel__body">
+      <div { ...rest } className={ classes }>
         { children }
       </div>
     );
