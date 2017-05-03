488aa5888870af67065bf2f0567a93c098d8d6f3
laurie <laurie@brandwatch.com>
2017-05-03 14:23:52 +0100

    Make labels in bar chart optional

--- src/charts/bar-chart/example/index.js
+++ src/charts/bar-chart/example/index.js
@@ -2,4 +2,5 @@ module.exports = [
   require('./barChartExampleSingleSeries').default,
   require('./BarChartTwoSeries').default,
   require('./BarChartTwoSeriesWithRatios').default,
+  require('./BarChartExampleNoLabels').default,
 ];
