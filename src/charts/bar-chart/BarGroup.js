55e40a8aa6e609e41171d9d579c670ac901b9062
laurie <laurie@brandwatch.com>
2017-05-04 10:46:14 +0100

    Remove labels props from bar component. Update tests

--- src/charts/bar-chart/BarGroup.js
+++ src/charts/bar-chart/BarGroup.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Small, Weak, Heading, Strong, Paragraph } from 'bw-axiom';
 import './BarGroup.css';
 
 export default class BarGroup extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
-    label: PropTypes.string,
-    ratio: PropTypes.string,
   };
 
   render() {
     const {
       children,
-      label,
-      ratio,
       ...rest
     } = this.props;
 
@@ -23,15 +18,6 @@ export default class BarGroup extends Component {
         <div className="ax-bar-chart__group-bars">
           { children }
         </div>
-        { (ratio || label) &&
-        <div className="ax-bar-chart__group-label">
-          { ratio && <Paragraph>
-            <Small><Weak>{ ratio }%</Weak></Small>
-          </Paragraph> }
-          { label && <Heading>
-            <Strong>{label}</Strong></Heading> }
-        </div>
-        }
       </div>
     );
   }
--- src/charts/bar-chart/BarGroup.test.js
+++ src/charts/bar-chart/BarGroup.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { BarGroup } from 'bw-axiom';
 
-function getComponent(props = { label: 'a_label' }) {
+function getComponent(props) {
   return renderer.create(
     <BarGroup { ...props } />
   );
@@ -11,12 +11,6 @@ function getComponent(props = { label: 'a_label' }) {
 describe('BarGroup', () => {
   it('renders with defaultProps', () => {
     const component = getComponent();
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders with ratio if provided', () => {
-    const component = getComponent({ ratio: '45' });
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
