b13fea701b6f96ee5d3aecb5b7cf7716b10a3197
Harry Hogg <harry@hogg.io>
2017-11-21 17:12:56 +0000

    fix(Button): Merged shape and size properties.
    BREAKING CHANGE:
    
    Added `shape` property and removed `circular` and `stadium` from Button
    
    ```jsx
    // Before
    <Button circular={ SIZE }></Button>
    <Button stadium={ SIZE></Button>
    
    // After
    <Button shape="circle" size={ SIZE }></Button>
    <Button shape="stadium" size={ SIZE }></Button>
    ```

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -12,8 +12,6 @@ export default class Button extends Component {
     active: PropTypes.bool,
     /** Content inserted into the Button */
     children: PropTypes.node.isRequired,
-    /** Size of circular shape */
-    circular: PropTypes.oneOf(['small', 'medium', 'large', 'huge']),
     /** Disable interaction behaviour */
     disabled: PropTypes.bool,
     /**
@@ -21,10 +19,10 @@ export default class Button extends Component {
      * with a value of `true` otherwise at one of the breakpoints specified.
      */
     full: PropTypes.oneOf([true, 'small', 'medium', 'large']),
+    /** Shape of the button */
+    shape: PropTypes.oneOf(['circle', 'rectangle', 'stadium']),
     /** Size of standard shape */
-    size: PropTypes.oneOf(['small', 'medium', 'large']),
-    /** Size of stadium shape */
-    stadium: PropTypes.oneOf(['small']),
+    size: PropTypes.oneOf(['small', 'medium', 'large', 'huge']),
     /** Style of the Button, that affects it's coloring and sizing */
     style: PropTypes.oneOf(['primary', 'secondary', 'tertiary', 'quaternary', 'caution']),
   };
@@ -34,8 +32,9 @@ export default class Button extends Component {
   };
 
   static defaultProps = {
-    style: 'primary',
+    shape: 'rectangle',
     size: 'medium',
+    style: 'primary',
   };
 
   render() {
@@ -43,9 +42,8 @@ export default class Button extends Component {
     const {
       active,
       children,
-      circular,
       disabled,
-      stadium,
+      shape,
       style,
       size,
       full,
@@ -54,17 +52,16 @@ export default class Button extends Component {
 
     const childrenArray = Children.toArray(children);
     const iconOnly = childrenArray.length === 1 && isComponent(childrenArray[0], ButtonIconRef);
-    const classes = classnames('ax-button', {
-      [`ax-button--${size}`]: size,
-      [`ax-button--${style}`]: style,
-      [`ax-button--circular-${circular}`]: circular,
-      [`ax-button--stadium-${stadium}`]: stadium,
-      'ax-button--active': active,
-      'ax-button--joined': joined,
-      'ax-button--icon-only': !circular && iconOnly,
-      'ax-button--full': full === true,
-      [`ax-button--full--${full}`]: typeof full === 'string',
-    });
+    const classes = classnames('ax-button',
+      `ax-button--${style}`,
+      `ax-button--${shape}-${size}`, {
+        'ax-button--active': active,
+        'ax-button--joined': joined,
+        'ax-button--icon-only': shape === 'rectangle' && iconOnly,
+        'ax-button--full': full === true,
+        [`ax-button--full--${full}`]: typeof full === 'string',
+      }
+    );
 
     const mappedChildren = childrenArray.map((child, index, array) =>
       !isComponent(child, ButtonIconRef) ? child : cloneElement(child, {
