e4df5eb00ce497d08bdc65537215f370024bb172
Sophie <sogando@brandwatch.com>
2018-06-11 09:18:18 +0200

    feat(Dialog): Adds option to add padding to DialogHeader, DialogBody, and DialogFooter

--- packages/axiom-components/src/Dialog/DialogFooter.js
+++ packages/axiom-components/src/Dialog/DialogFooter.js
@@ -1,18 +1,29 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import classnames from 'classnames';
 import Base from '../Base/Base';
 
 export default class DialogFooter extends Component {
   static propTypes = {
     /** Footer content inside the Dialog, a good place for some buttons */
     children: PropTypes.node,
+    /** Padding size applied to the footer area */
+    padding: PropTypes.oneOf(['none', 'small', 'large']),
+  };
+
+  static defaultProps = {
+    padding: 'small',
   };
 
   render() {
-    const { children, ...rest } = this.props;
+    const { children, padding, ...rest } = this.props;
+
+    const classes = classnames('ax-dialog__footer',
+      `ax-dialog__footer--padding-${ padding }`
+    );
 
     return (
-      <Base { ...rest } className="ax-dialog__footer">
+      <Base { ...rest } className={ classes }>
         { children }
       </Base>
     );
