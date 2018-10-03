9db08921c9d628cf15e58eb4b43ef6ef98e57af5
Stefan Buck <stefanb@brandwatch.com>
2018-10-03 22:15:14 +0200

    Update to babel 7

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -3,4 +3,4 @@ set -e
 
 npx lerna exec --parallel -- rm -rf dist
 npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist
-npx lerna exec --parallel -- npx babel dist -d dist
+npx lerna exec --parallel -- npx babel dist -d dist --config-file $(pwd)/babel.config.js
