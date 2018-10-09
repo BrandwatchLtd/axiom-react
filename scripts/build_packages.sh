3e83a2a3b9954715eba8c9cd6089d7620656d027
Jon Linklater-Johnson <jonl@brandwatch.com>
2018-10-09 18:04:19 +0100

    chore(build): Pre-process CSSNext style nesting
    But do not pre-process custom properties as these are transpiled by consuming applications

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -4,3 +4,4 @@ set -e
 npx lerna exec --parallel -- rm -rf dist
 npx lerna exec --parallel -- rsync -a --prune-empty-dirs --exclude '__snapshots__/*' --exclude '*.test.js' src/* dist
 npx lerna exec --parallel -- npx babel dist -d dist
+npx postcss-cli packages/**/dist/**/*.css --config ../postcss.prod.config.js --replace
