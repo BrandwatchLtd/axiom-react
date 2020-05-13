a7383d9d3af453ddaf1e582823f71cad8d284f59
laurie <laurie@brandwatch.com>
2020-05-13 15:36:48 +0100

    Update publish site script to work locally

--- scripts/publish_site.sh
+++ scripts/publish_site.sh
@@ -1,41 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    exit 0
-fi
-
-curl ci-utils.bwcom.io/gcloud/install | bash
-curl ci-utils.bwcom.io/gcloud/auth | bash
-
 cd public;
 
 # selectively clear the bucket, gsutil throws if it's already empty
-! gsutil -m rm -fr gs://$STORAGE_BUCKET/docs
-! gsutil -m rm -fr gs://$STORAGE_BUCKET/assets
-! gsutil rm -f gs://$STORAGE_BUCKET/index.html
+! gsutil -m rm -fr gs://axiom.platform-prod.gcp0.bwcom.net/docs
+! gsutil -m rm -fr gs://axiom.platform-prod.gcp0.bwcom.net/assets
+! gsutil rm -f gs://axiom.platform-prod.gcp0.bwcom.net/index.html
 
 # selectively copy everything into the bucket
-gsutil cp index.html gs://$STORAGE_BUCKET
-gsutil -m cp -r assets gs://$STORAGE_BUCKET
-gsutil -m cp -r docs gs://$STORAGE_BUCKET
+gsutil cp index.html gs://axiom.platform-prod.gcp0.bwcom.net
+gsutil -m cp -r assets gs://axiom.platform-prod.gcp0.bwcom.net
+gsutil -m cp -r docs gs://axiom.platform-prod.gcp0.bwcom.net
 
 # set the index html as the default, and forward all errors to the app
-gsutil -m web set -m index.html -e index.html gs://$STORAGE_BUCKET
+gsutil -m web set -m index.html -e index.html gs://axiom.platform-prod.gcp0.bwcom.net
 
 # Set public access to bucket
-gsutil -m acl -r ch -u AllUsers:R gs://$STORAGE_BUCKET
+gsutil -m acl -r ch -u AllUsers:R gs://axiom.platform-prod.gcp0.bwcom.net
 
 # Headers on everything in the bucket
-gsutil -m setmeta -h "Cache-Control:max-age=31536000" gs://$STORAGE_BUCKET/assets/**
+gsutil -m setmeta -h "Cache-Control:max-age=31536000" gs://axiom.platform-prod.gcp0.bwcom.net/assets/**
 
 # GZIP Headers
-gsutil -m setmeta -h "Content-Encoding:gzip" gs://$STORAGE_BUCKET/**.js
-gsutil -m setmeta -h "Content-Encoding:gzip" gs://$STORAGE_BUCKET/**.css
-gsutil -m setmeta -h "Content-Encoding:gzip" gs://$STORAGE_BUCKET/**.html
+gsutil -m setmeta -h "Content-Encoding:gzip" gs://axiom.platform-prod.gcp0.bwcom.net/**.js
+gsutil -m setmeta -h "Content-Encoding:gzip" gs://axiom.platform-prod.gcp0.bwcom.net/**.css
+gsutil -m setmeta -h "Content-Encoding:gzip" gs://axiom.platform-prod.gcp0.bwcom.net/**.html
 
 # Override headers for html files
 gsutil -m setmeta \
   -h "Content-Type:text/html" \
   -h "Cache-Control:no-cache, max-age=0" \
-  gs://$STORAGE_BUCKET/**.html
+  gs://axiom.platform-prod.gcp0.bwcom.net/**.html
